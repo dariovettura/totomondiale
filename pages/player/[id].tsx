150e9fbd959c37ee3f836f1a1808ab82bb882617
dariovettura <d.vettura.wd@gmail.com>
2022-11-17 22:29:17 +0100

    add: tutte le scommesse

--- pages/player/[id].tsx
+++ pages/player/[id].tsx
@@ -6,6 +6,7 @@ import { imageOptimizer } from "next/dist/server/image-optimizer";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
+import teams from "../../teams/teams";
 
 interface Props {
   posts?: any[];
@@ -16,8 +17,15 @@ const Player: NextPage<Props> = ({ posts, infos }) => {
   const router = useRouter();
   const { id } = router.query;
 
-  const [item, setItem] = React.useState<any[]>([]);
+  const [myRes, setMyRes] = React.useState<any[]>([]);
+  const [myOffRes, setMyOffRes] = React.useState<any>({});
   const [name, setName] = React.useState<any>([]);
+
+  const groups = ["A", "B", "C", "D", "E", "F", "G", "H"];
+  const quarters = [1, 2, 3, 4, 5, 6, 7, 8];
+  const semifinals = [1, 2, 3, 4];
+  const thirdPlace = [1, 2];
+  const finals = [1, 2];
 
   async function navigate() {
     router.push(
@@ -33,9 +41,14 @@ const Player: NextPage<Props> = ({ posts, infos }) => {
     return axios.post("/api/getMyResults", { data: id }).then((res) => {
       console.log(res);
       setName(res.data.billing.first_name);
-      setItem(JSON.parse(res.data.customer_note).myResult);
+      setMyRes(JSON.parse(res.data.customer_note).myResult);
+      setMyOffRes(JSON.parse(res.data.customer_note).myPlayOffResults);
     });
   };
+
+  const getTeamName = (id: any) => {
+    return teams?.find(el => el.team_id == id)?.name
+  }
 
   React.useEffect(() => {
     getMyResults();
@@ -49,17 +62,16 @@ const Player: NextPage<Props> = ({ posts, infos }) => {
             marginTop: "60px",
             marginBottom: "60px",
             width: "100vw",
-            height: "100%",
             display: "flex",
             flexDirection: "column",
-            gap: 20,
+            gap: "20px",
             alignItems: "center",
             justifyContent: "center",
           }}
         >
           <span style={{ fontSize: "30px", fontWeight: "bold" }}>{name}</span>
 
-          {item.map((el, i) => (
+          {myRes.map((el, i) => (
             <div key={i} style={{ display: "flex", flexDirection: "column" }}>
               <div>
                 <img
@@ -85,6 +97,108 @@ const Player: NextPage<Props> = ({ posts, infos }) => {
               </div>
             </div>
           ))}
+
+          {groups.map((el:any, i) => (
+            <div key={i}>
+              <h3>Girone {el} passano (2pt per squadra)</h3>
+              <div>
+                <div>
+                  <label htmlFor={`sq1-{el}`}>Girone {el} Squadra 1</label>
+                  <span
+                   style={{
+                    fontSize: "30px",
+                    fontWeight: "bold",
+                    marginLeft: "20px",
+                  }}>{getTeamName(myOffRes[`girone${el}1`])}</span>
+                
+                  
+                </div>
+                <div>
+                  <label htmlFor={`sq2-{el}`}>Girone {el} Squadra 2</label>
+                  <span
+                   style={{
+                    fontSize: "30px",
+                    fontWeight: "bold",
+                    marginLeft: "20px",
+                  }}>{getTeamName(myOffRes[`girone${el}2`])}</span>
+                
+                </div>
+              </div>
+            </div>
+          ))}
+
+          <h3>Quarti passano (2pt per squadra)</h3>
+          {quarters.map((el, i) => (
+            <div key={i}>
+              <label htmlFor={`sq-{el}`}>Quarti Squadra {el}</label>
+              <span
+               style={{
+                fontSize: "30px",
+                fontWeight: "bold",
+                marginLeft: "20px",
+              }}>{getTeamName(myOffRes[`quarti${el}`])}</span>
+            </div>
+          ))}
+
+          <h3>Semifinali passano (2pt per squadra)</h3>
+          {semifinals.map((el, i) => (
+            <div key={i}>
+              <label htmlFor={`sq-{el}`}>Semi Squadra {el}</label>
+              <span
+               style={{
+                fontSize: "30px",
+                fontWeight: "bold",
+                marginLeft: "20px",
+              }}>{getTeamName(myOffRes[`semi${el}`])}</span>
+           
+            </div>
+          ))}
+
+          <h3>Finale 3zo posto (6pt per squadra)</h3>
+          {thirdPlace.map((el, i) => (
+            <div key={i}>
+              <label htmlFor={`sq-{el}`}>Finale 3/4 Squadra {el}</label>
+              <span
+               style={{
+                fontSize: "30px",
+                fontWeight: "bold",
+                marginLeft: "20px",
+              }}>{getTeamName(myOffRes[`terzo${el}`])}</span>
+            </div>
+          ))}
+
+          <h3>Finale (8pt per squadra)</h3>
+          {finals.map((el, i) => (
+            <div key={i}>
+              <label htmlFor={`sq-{el}`}>Finale Squadra {el}</label>
+              <span
+               style={{
+                fontSize: "30px",
+                fontWeight: "bold",
+                marginLeft: "20px",
+              }}>{getTeamName(myOffRes[`finale${el}`])}</span>
+            </div>
+          ))}
+
+          <h3>Vincitrice (12 pt)</h3>
+          <label htmlFor={`winner`}>Vincitrice</label>
+          <span
+           style={{
+            fontSize: "30px",
+            fontWeight: "bold",
+            marginLeft: "20px",
+          }}>{getTeamName(myOffRes[`vincitrice`])}</span>
+        
+
+          <h3>Squadra con capocannoniere (5pt)</h3>
+          <label htmlFor={`goleador`}>Sq Capocannoniere</label>
+          <span
+           style={{
+            fontSize: "30px",
+            fontWeight: "bold",
+            marginLeft: "20px",
+          }}>{getTeamName(myOffRes[`capocannoniere`])}</span>
+        
         </div>
       </div>
     </>
