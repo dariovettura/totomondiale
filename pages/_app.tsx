819417977ac97add5b6734d8d0b53c5ee603b78f
dariovettura <d.vettura.wd@gmail.com>
2022-11-18 16:58:01 +0100

    test header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,7 @@ function App({ Component, pageProps }: AppProps) {
         }
       `}</style>
       {" "}
-      <Header></Header>
+      {/* <Header></Header> */}
       <Component  {...pageProps} />
     </main>
   );
