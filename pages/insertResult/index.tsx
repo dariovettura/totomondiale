f0a641e15960361bbb3fa3fd1ca3de6ae06cff49
dariovettura <d.vettura.wd@gmail.com>
2022-11-18 18:00:14 +0100

    fix

--- pages/insertResult/index.tsx
+++ pages/insertResult/index.tsx
@@ -1,6 +1,5 @@
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
-import "antd/dist/antd.css";
 import InputLabel from '@mui/material/InputLabel';
 import Snackbar from "@mui/material/Snackbar";
 import Backdrop from "@mui/material/Backdrop";
